44fdaa6d8ca995ee9385a363d217b90e84d47a32
Dimas Alf <dimas.alf002@gmail.com>
2025-08-21 00:33:14 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+function playClick() {
+    const sound = document.getElementById("clickSound");
+    sound.currentTime = 0; // reset biar bisa klik cepat2
+    sound.play();
+}
+
+function playTombol() {
+    const sound = document.getElementById("clickSoundSamaDengan");
+    sound.currentTime = 0; // reset biar bisa klik cepat2
+    sound.play();
+}
+
+
 function appenValue(value){
+    playClick();
     document.getElementById('display').value += value;
 }
 
+
 function clearDisplay(){
+    playClick();
     document.getElementById('display').value = '';
 }
 
+
 function calculate(){
+    playTombol()
     const display = document.getElementById('display');
     try{
         display.value = eval(display.value);
